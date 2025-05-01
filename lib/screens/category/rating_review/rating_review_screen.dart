b612425ea1205b95937b3f5a0ce8419fbd107cea
Arya Jagadditha <rhea@Aryas-MacBook-Air.local>
2025-05-01 14:18:23 +0700

    feat: finish creating files template for all screen

---
+++ lib/screens/category/rating_review/rating_review_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RatingReviewScreen extends StatelessWidget {
+  const RatingReviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
